e16f77ddf83697dde1a06c67c72f92dcc6a2e010
Sarad Singh <theArkein.dev@gmail.com>
2022-12-18 09:48:38 +0545

    chore: Config prettier and prettier-plugin-tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <main>
-        <h3 className="text-white">Welcome to my portfolio</h3>
+        <h3 className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+          Welcome to my portfolio
+        </h3>
       </main>
     </>
   );
